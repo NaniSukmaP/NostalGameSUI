1cb0508071451a66d8a59c12817b7589d9ba319f
NaniSukmaP <luckyputri98@gmail.com>
2020-09-02 22:35:43 +0700

    Initial Commit

---
+++ NostalGameSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NostalGameSUI
+//
+//  Created by Nani Sukma Putri Pratama on 02/09/20.
+//  Copyright © 2020 Nani Sukma Putri Pratama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
